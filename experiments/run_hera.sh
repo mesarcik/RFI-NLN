0e2d7125725e5b1e251c09077edafc5fa82882ac
Misha Mesarcik <m.mesarcik@uva.nl>
2022-01-31 11:02:10 +0100

    stragglers

--- experiments/run_hera.sh
+++ experiments/run_hera.sh
@@ -7,33 +7,31 @@ seed=$(openssl rand -hex 3)
 d=$(date +'%m-%d-%Y-%I-%M_')
 atype=MISO
 
-for ld in 16 32 64 
+for ld in 32 64 128 256
 do
-		for patch in 16 32 64
+		for patch in 8 16 32 64 128
 		do
-				for rfi in rfi_dtv rfi_stations rfi_impulse 
-				do
-						python -u main.py -limit $limit\
-										  -data HERA\
-										  -data_path /home/mmesarcik/data/HERA/HERA_6_28-01-2022_MIXED.pkl\
-										  -anomaly_class rfi\
-										  -rfi_threshold 666\
-										  -rfi $rfi\
-										  -anomaly_type $atype\
-										  -percentage_anomaly 0\
-										  -epochs $epochs \
-										  -latent_dim $ld \
-										  -patches True \
-										  -crop_x $patch\
-										  -crop_y $patch\
-										  -patch_x $patch \
-										  -patch_y $patch \
-										  -patch_stride_x $patch \
-										  -patch_stride_y $patch \
-										  -neighbors 1 2 5 10 \
-										  -algorithm knn\
-										  -seed $d$seed | tee -a hera.log 
-				done 
+				python -u main.py -limit $limit\
+								  -data HERA\
+								  -data_path /home/mmesarcik/data/HERA/HERA_6_28-01-2022_MIXED.pkl\
+								  -anomaly_class rfi\
+								  -rfi_threshold 666\
+								  #-rfi $rfi\
+								  -anomaly_type $atype\
+								  -percentage_anomaly 0\
+								  -epochs $epochs \
+								  -latent_dim $ld \
+								  -patches True \
+								  -crop_x $patch\
+								  -crop_y $patch\
+								  -patch_x $patch \
+								  -patch_y $patch \
+								  -patch_stride_x $patch \
+								  -patch_stride_y $patch \
+								  -neighbors 1 2 5 10 \
+								  -alpha 0 0.25 0.5 0.75 0.9 1.0\
+								  -algorithm knn\
+								  -seed $d$seed | tee -a hera.log 
 		done
 done
 
